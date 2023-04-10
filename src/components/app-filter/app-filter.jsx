850520cfe59a78a37fdd414cc4f2c662e817cf59
Eugene Skl <zheksem@gmail.com>
2023-04-10 17:14:45 +0300

    Employees counting v1.2.0 all fitches are working

--- src/components/app-filter/app-filter.jsx
+++ src/components/app-filter/app-filter.jsx
@@ -1,10 +1,30 @@
 
 import './app-filter.css'
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+	const buttonsData = [
+		{ name: 'all', label: 'All staff' },
+		{ name: 'rise', label: 'Employee up for promotion' },
+		{ name: 'moreThen1k', label: 'Salary over 1k$' },
+	];
+
+	const buttons = buttonsData.map(({ name, label }) => {
+		const active = props.filter === name;
+		const clazz = active ? 'btn-light' : 'btn-outline-light';
+		return (
+			<button className={`btn ${clazz}`}
+				type='button'
+				key={name}
+				onClick={() => props.onFilterSelect(name)}>
+				{label}
+			</button>
+		)
+	})
+
 	return (
 		<div className="btn-group">
-			<button
+			{buttons}
+			{/* <button
 				className="btn btn-light"
 				type='button'>
 				All staff
@@ -13,11 +33,12 @@ const AppFilter = () => {
 				className="btn btn-outline-light"
 				type='button'>
 				Employee up for promotion
-			</button><button
+			</button>
+			<button
 				className="btn btn-outline-light"
 				type='button'>
 				Salary over 1k$
-			</button>
+			</button> */}
 		</div>
 	)
 }
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -16,7 +16,9 @@ class App extends Component {
 				{ name: 'Vlada', salary: 5500, increase: false, rise: true, id: 1 },
 				{ name: 'Jack', salary: 100500, increase: true, rise: false, id: 2 },
 				{ name: 'Eugene', salary: 50, increase: false, rise: false, id: 3 },
-			]
+			],
+			term: '',
+			filter: 'all'
 		}
 		this.maxId = 4;
 	}
@@ -97,19 +99,50 @@ class App extends Component {
 	// 	}
 	// })
 
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+
+		return items.filter(item => {
+			return item.name.indexOf(term) > -1
+		})
+	}
+
+	onUpdateSearch = (term) => {
+		this.setState({ term });
+	}
+
+	filterPost = (items, filter) => {
+		switch (filter) {
+			case 'rise':
+				return items.filter(item => item.rise);
+			case 'moreThen1k':
+				return items.filter(item => item.salary > 1000);
+			default:
+				return items
+		}
+	}
+
+	onFilterSelect = (filter) => {
+		this.setState({ filter });
+	}
+
 	render() {
+		const { data, term, filter } = this.state;
 		const employees = this.state.data.length;
 		const increased = this.state.data.filter(item => item.increase).length;
+		const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 		return (
 			<div className="app">
 				<AppInfo employees={employees}
 					increased={increased} />
 				<div className="search-panel">
-					<SearchPanel />
-					<AppFilter />
+					<SearchPanel onUpdateSearch={this.onUpdateSearch} />
+					<AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
 				</div>
 				<EmployeesList
-					data={this.state.data}
+					data={visibleData}
 					onDelete={this.deleteItem}
 					onToggleProp={this.onToggleProp}
 				/>
